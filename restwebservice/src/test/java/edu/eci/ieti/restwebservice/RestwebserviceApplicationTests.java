b82500c485f2c4c465302563993b83e8c80c61f0
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-08-15 13:09:59 -0500

    Spring initializr: Initial project created

---
+++ restwebservice/src/test/java/edu/eci/ieti/restwebservice/RestwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.restwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
